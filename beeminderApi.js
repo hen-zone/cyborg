39208253f44f64a289f7c9ed63d1b65dfc6e4121
Henry Greville <spam@ironi.st>
2016-08-24 22:03:09 +0800

    Send a notification to go with every beeminder update

--- beeminderApi.js
+++ beeminderApi.js
@@ -1,11 +1,16 @@
 import request from "request-promise";
+import {sendNotification} from './iftttNotificationsApi'
 
 const BEEMINDER_AUTH_TOKEN = 'jUyVNQvxwe9gxzfapmu1';
 const BEEMINDER_USER_NAME = 'hypertexture';
 
 export async function incrementBeeminderGoal(goalId) {
-    return await request({
+    const result = await request({
         method: 'POST',
         uri: `https://www.beeminder.com/api/v1/users/${BEEMINDER_USER_NAME}/goals/${goalId}/datapoints.json?value=1&auth_token=${BEEMINDER_AUTH_TOKEN}`
     });
+
+    await sendNotification(`🐝 Updated the ${goalId} goal.`);
+
+    return result;
 }
