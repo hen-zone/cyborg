390887e0f65c953f34d9ac31720a026d6138da8a
@hen <hello@hen.zone>
2017-07-12 12:36:18 +0800

    Add needle drop to track sources

--- spotify.js
+++ spotify.js
@@ -242,6 +242,7 @@ export async function scanInboxes(req) {
         ['pitchforkOfficialTracks', 'pitchfork', '7q503YgioHAbo1iOIa67M8'],
         ['pitchforkUnofficialAlbums', 'kenove', '6QdRN6dPnook9KPezrggaO'],
         ['jjjHitList', 'triple.j.abc', '7vFQNWXoblEJXpbnTuyz76'],
+        ['needleDropTracks', 'szymonczarnowski', '1h9vpwvGP5Pr2qvAzNCjOK'],
         // ['livvyDiscover', 'spotify', '37i9dQZEVXcJP0NgDg2X0T'],
         // ['pitchforkUnofficialTracks', 'szymonczarnowski', '2LkZTDKWPelJv7HNY9rQV7'],
         // ['izaakDiscover', 'spotify', '37i9dQZEVXcDc5DQak61yg'],
